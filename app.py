d93d1f33176d966fbc4f2a0d64f653e16eb73bac
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2022-02-03 00:04:45 +0100

    Use concurrency to optimize copying documents

--- app.py
+++ app.py
@@ -17,11 +17,13 @@
 
 """Sync Thoth documents to an S3 API compatible remote."""
 
+import threading
 import os
 import json
 import logging
 import subprocess
 from tempfile import NamedTemporaryFile
+from concurrent.futures.thread import ThreadPoolExecutor
 
 import click
 from thoth.common import init_logging
@@ -29,6 +31,7 @@ from thoth.storages import __version__ as __thoth_storages_version__
 from thoth.storages import AnalysisByDigest
 from thoth.storages import AnalysisResultsStore
 from thoth.storages import SolverResultsStore
+from thoth.storages.result_base import ResultStorageBase
 
 from prometheus_client import CollectorRegistry, Gauge, Counter, push_to_gateway
 
@@ -39,6 +42,8 @@ __component_version__ = f"{__version__}+storages{__thoth_storages_version__}"
 
 init_logging()
 _LOGGER = logging.getLogger("thoth.document_sync")
+
+_DEFAULT_CONCURRENCY = 16
 _THOTH_DEPLOYMENT_NAME = os.environ["THOTH_DEPLOYMENT_NAME"]
 _THOTH_METRICS_PUSHGATEWAY_URL = os.getenv("PROMETHEUS_PUSHGATEWAY_URL")
 
@@ -57,7 +62,68 @@ _METRIC_DOCUMENTS_SYNC_NUMBER = Counter(
     registry=prometheus_registry,
 )
 
+_METRIC_DOCUMENTS_SYNC_NUMBER_THREAD_LOCK = threading.Lock()
+
 _METRIC_INFO.labels(_THOTH_DEPLOYMENT_NAME, __component_version__).inc()
+
+
+def _sync_worker(adapter: ResultStorageBase, document_id: str, dst: str, *, force: bool = False) -> None:
+    """Sync document logic."""
+    destination = f"{dst}/{adapter.RESULT_TYPE}/{document_id}"
+    proc = subprocess.run(["aws", "s3", "ls", destination], capture_output=True)
+
+    if proc.returncode == 0 and not force:
+        _LOGGER.info("Document %r is already present", document_id)
+        with _METRIC_DOCUMENTS_SYNC_NUMBER_THREAD_LOCK:
+            _METRIC_DOCUMENTS_SYNC_NUMBER.labels(
+                sync_type="skipped",
+                env=_THOTH_DEPLOYMENT_NAME,
+                version=__component_version__,
+                result_type=adapter.RESULT_TYPE,
+            ).inc()
+        return
+
+    _LOGGER.info("Copying document to %r", destination)
+
+    document = adapter.retrieve_document(document_id)
+    with NamedTemporaryFile(mode="w") as temp:
+        temp.write(json.dumps(document, sort_keys=True, indent=2))
+        temp.flush()
+
+        proc = subprocess.run(
+            [
+                "aws",
+                "s3",
+                "cp",
+                temp.name,
+                destination,
+            ],
+            capture_output=True,
+        )
+        if proc.returncode != 0:
+            _LOGGER.error(
+                "Failed to copy %r to %r (exit code %d): %s",
+                document_id,
+                destination,
+                proc.returncode,
+                proc.stderr,
+            )
+            with _METRIC_DOCUMENTS_SYNC_NUMBER_THREAD_LOCK:
+                _METRIC_DOCUMENTS_SYNC_NUMBER.labels(
+                    sync_type="error",
+                    env=_THOTH_DEPLOYMENT_NAME,
+                    version=__component_version__,
+                    result_type=adapter.RESULT_TYPE,
+                ).inc()
+        else:
+            _LOGGER.info("Document %r successfully copied to %r", document_id, destination)
+            with _METRIC_DOCUMENTS_SYNC_NUMBER_THREAD_LOCK:
+                _METRIC_DOCUMENTS_SYNC_NUMBER.labels(
+                    sync_type="success",
+                    env=_THOTH_DEPLOYMENT_NAME,
+                    version=__component_version__,
+                    result_type=adapter.RESULT_TYPE,
+                ).inc()
 
 
 @click.command()
@@ -68,13 +134,20 @@ _METRIC_INFO.labels(_THOTH_DEPLOYMENT_NAME, __component_version__).inc()
     help="Perform force copy of documents.",
     envvar="THOTH_DOCUMENT_SYNC_FORCE",
 )
+@click.option(
+    "--concurrency",
+    type=int,
+    help="Number of concurrent workers syncing documents.",
+    envvar="THOTH_DOCUMENT_SYNC_CONCURRENCY",
+    default=_DEFAULT_CONCURRENCY,
+)
 @click.argument(
     "dst",
     envvar="THOTH_DOCUMENT_SYNC_DST",
     type=str,
     metavar="s3://thoth/data/deployment",
 )
-def sync(dst: str, debug: bool = False, force: bool = False) -> None:
+def sync(dst: str, debug: bool = False, force: bool = False, concurrency: int = _DEFAULT_CONCURRENCY) -> None:
     """Sync Thoth data to a remote with an S3 compatible interface."""
     if debug:
         _LOGGER.setLevel(logging.DEBUG)
@@ -82,86 +155,26 @@ def sync(dst: str, debug: bool = False, force: bool = False) -> None:
 
     _LOGGER.info("Running document syncing job in version %r", __component_version__)
 
-    number_synced_documents = 0
-    number_unsynced_documents = 0
-    number_skipped_documents = 0
-
     adapters = (AnalysisByDigest, AnalysisResultsStore, SolverResultsStore)
-    for adapter_class in adapters:
-        adapter = adapter_class()
-        adapter.connect()
+    try:
+        for adapter_class in adapters:
+            adapter = adapter_class()
+            adapter.connect()
 
-        for document_id in adapter.get_document_listing():
-            destination = f"{dst}/{adapter.RESULT_TYPE}/{document_id}"
-            proc = subprocess.run(["aws", "s3", "ls", destination], capture_output=True)
-
-            if proc.returncode == 0 and not force:
-                _LOGGER.info("Document %r is already present", document_id)
-                number_skipped_documents += 1
-                continue
-
-            _LOGGER.info("Copying document to %r", destination)
-
-            document = adapter.retrieve_document(document_id)
-            with NamedTemporaryFile(mode="w") as temp:
-                temp.write(json.dumps(document, sort_keys=True, indent=2))
-                temp.flush()
-
-                proc = subprocess.run(
-                    [
-                        "aws",
-                        "s3",
-                        "cp",
-                        temp.name,
-                        destination,
-                    ],
-                    capture_output=True,
+            with ThreadPoolExecutor(max_workers=concurrency) as executor:
+                for document_id in adapter.get_document_listing():
+                    executor.submit(_sync_worker, adapter, document_id, dst, force=force)
+    finally:
+        if _THOTH_METRICS_PUSHGATEWAY_URL:
+            try:
+                _LOGGER.debug(f"Submitting metrics to Prometheus pushgateway {_THOTH_METRICS_PUSHGATEWAY_URL}")
+                push_to_gateway(
+                    _THOTH_METRICS_PUSHGATEWAY_URL,
+                    job="document-sync-job",
+                    registry=prometheus_registry,
                 )
-
-                if proc.returncode != 0:
-                    _LOGGER.error(
-                        "Failed to copy %r to %r (exit code %d): %s",
-                        document_id,
-                        destination,
-                        proc.returncode,
-                        proc.stderr,
-                    )
-                    number_unsynced_documents += 1
-                else:
-                    number_synced_documents += 1
-            break
-
-        _METRIC_DOCUMENTS_SYNC_NUMBER.labels(
-            sync_type="success",
-            env=_THOTH_DEPLOYMENT_NAME,
-            version=__component_version__,
-            result_type=adapter_class.RESULT_TYPE,
-        ).inc(number_synced_documents)
-
-        _METRIC_DOCUMENTS_SYNC_NUMBER.labels(
-            sync_type="error",
-            env=_THOTH_DEPLOYMENT_NAME,
-            version=__component_version__,
-            result_type=adapter_class.RESULT_TYPE,
-        ).inc(number_unsynced_documents)
-
-        _METRIC_DOCUMENTS_SYNC_NUMBER.labels(
-            sync_type="skipped",
-            env=_THOTH_DEPLOYMENT_NAME,
-            version=__component_version__,
-            result_type=adapter_class.RESULT_TYPE,
-        ).inc(number_skipped_documents)
-
-    if _THOTH_METRICS_PUSHGATEWAY_URL:
-        try:
-            _LOGGER.debug(f"Submitting metrics to Prometheus pushgateway {_THOTH_METRICS_PUSHGATEWAY_URL}")
-            push_to_gateway(
-                _THOTH_METRICS_PUSHGATEWAY_URL,
-                job="document-sync-job",
-                registry=prometheus_registry,
-            )
-        except Exception as e:
-            _LOGGER.exception(f"An error occurred pushing the metrics: {str(e)}")
+            except Exception as e:
+                _LOGGER.exception(f"An error occurred pushing the metrics: {str(e)}")
 
 
 __name__ == "__main__" and sync()
